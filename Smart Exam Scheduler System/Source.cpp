cb82af20898c156192a72ee21f6fecfd4a55ed6a
mawada sweis <mawada.sweis19@gmail.com>
2021-03-15 20:49:01 +0200

    update graph structure

--- Smart Exam Scheduler System/Source.cpp
+++ Smart Exam Scheduler System/Source.cpp
@@ -21,11 +21,26 @@ struct exam
 
 struct graph {
 	vector<exam> graph_1,
-		graph_2,
-		graph_3;
+		graph_2;
 	
+	// Function to print the suggest for each exam in each graph
+	void print_graph() {
+		cout << "\n*************************************\n";
+		cout << "Graph 1 : \n";
+		for (size_t i = 0; i < graph_1.size(); i++)
+		{
+			cout << graph_1[i].exam_order << "\t";
+		}
+		cout << "\nGraph 2 : \n";
+		for (size_t i = 0; i < graph_2.size(); i++)
+		{
+			cout << graph_2[i].exam_order << "\t";
+		}
+		cout << "\n*************************************\n";
+	}
+
 	// Function to assign exams to each graph according to time constraint
-	void update_queue(exam* E) 
+	void update_queue(exam* E)
 	{
 		for (int j = 0; j < length; j++)
 		{
@@ -33,27 +48,26 @@ struct graph {
 			{
 			case 0:
 			case 2:
-				insert(graph_1, E[j]);
-				break;
 			case 3:
 			case 8:
 			case 9:
-				insert(graph_2, E[j]);
+				insert(graph_1, E[j]);
 				break;
 			case 1:
 			case 4:
 			case 5:
 			case 6:
 			case 7:
-				insert(graph_3, E[j]);
+				insert(graph_2, E[j]);
 				break;
 			}
 		}
+		print_graph();
 	}
 	
 	// Function to heapify the tree
-	void heapify(vector<exam> &graph,
-		int i) 
+	void heapify(vector<exam>& graph,
+		int i)
 	{
 		// Find the largest among root, left child and right child
 		int largest = i,
@@ -92,46 +106,6 @@ struct graph {
 		}
 	}
 
-	// Function to print the suggest for each exam in each graph
-	void print_graph() {
-		for (size_t i = 0; i < length; i++)
-		{
-			cout << "E" << graph_1[i].exam_order << " -> (t";
-			switch (i + 1) {
-			case 1: 
-				cout << get<0>(graph_1[i].suggest) << "," << get<1>(graph_1[i].suggest) << ")\n";
-				break;
-			case 2:
-				cout << get<0>(graph_3[i-1].suggest) << "," << get<1>(graph_3[i-1].suggest) << ")\n";
-				break;
-			case 3:
-				cout << get<0>(graph_1[i - 1].suggest) << "," << get<1>(graph_1[i - 1].suggest) << ")\n";
-				break;
-			case 4:
-				cout << get<0>(graph_2[i - 3].suggest) << "," << get<1>(graph_2[i - 3].suggest) << ")\n";
-				break;
-			case 5:
-				cout << get<0>(graph_3[i - 3].suggest) << "," << get<1>(graph_3[i - 3].suggest) << ")\n";
-				break;
-			case 6:
-				cout << get<0>(graph_3[i - 3].suggest) << "," << get<1>(graph_3[i - 3].suggest) << ")\n";
-				break;
-			case 7:
-				cout << get<0>(graph_3[i - 3].suggest) << "," << get<1>(graph_3[i - 3].suggest) << ")\n";
-				break;
-			case 8:
-				cout << get<0>(graph_3[i - 3].suggest) << "," << get<1>(graph_3[i - 3].suggest) << ")\n";
-				break;
-			case 9:
-				cout << get<0>(graph_2[i - 7].suggest) << "," << get<1>(graph_2[i - 7].suggest) << ")\n";
-				break;
-			case 10:
-				cout << get<0>(graph_2[i - 7].suggest) << "," << get<1>(graph_2[i - 7].suggest) << ")\n";
-				break;
-			}
-			cout << endl;
-		}
-	}
 };
 
 // Function to update the domain list for each exam in his constraint
@@ -235,22 +209,6 @@ void pop_domain(graph& all_graph,
 
 	}
 
-	for (size_t i = 0; i < all_graph.graph_3.size(); i++)
-	{
-		for (size_t k = 0; k < all_graph.graph_3[i].domain_list.size(); k++)
-		{
-			if (all_graph.graph_3[i].domain_list[k] == suggest_to_remove)
-			{
-				for (size_t j = k; j < all_graph.graph_3[i].domain_list.size() - 1; j++)
-				{
-					all_graph.graph_3[i].domain_list[j].swap(all_graph.graph_3[i].domain_list[j + 1]);
-				}
-				all_graph.graph_3[i].domain_list.pop_back();
-			}
-		}
-
-	}
-
 }
 
 // Function to test if each suggest in the graph is unique value
@@ -270,7 +228,8 @@ bool is_unique(vector<exam> this_graph) {
 }
 
 // Function to delete the wrong random suggest from each exam in his graph 
-void pop_unique_suggest(vector<exam> this_graph) 
+void pop_unique_suggest(vector<exam> this_graph,
+	exam* e) 
 {
 	for (size_t i = 0; i < this_graph.size(); i++)
 	{
@@ -288,9 +247,21 @@ void pop_unique_suggest(vector<exam> this_graph)
 	}
 }
 
+// Function to find index of spacific exam order
+int index_of(vector<exam> g, int element) {
+	for (int i = 0; i < g.size(); i++)
+	{
+		if (g[i].exam_order == element) {
+			return i;
+		}
+	}
+	return -1;
+}
+
 // Function to assign each variable to a specific value from its domain
-void backtracking(graph &all_graph) 
-{	
+void backtracking(graph &all_graph,
+	exam* e) 
+{
 	bool to_break = false;
 	int i = all_graph.graph_1.size() - 1;
 	do 
@@ -301,22 +272,34 @@ void backtracking(graph &all_graph)
 			all_graph.graph_1[j].suggest =
 				all_graph.graph_1[j].domain_list[rand() % all_graph.graph_1[j].domain_list.size()];
 		}
-		
-		if (get<0>(all_graph.graph_1[i].suggest) > get<0>(all_graph.graph_1[i - 1].suggest) &&
+		if (get<0>(all_graph.graph_1.at(index_of(all_graph.graph_1, 3)).suggest) > get<0>(all_graph.graph_1.at(index_of(all_graph.graph_1, 1)).suggest) &&
+			all_graph.graph_1.at(index_of(all_graph.graph_1, 9)).suggest < all_graph.graph_1.at(index_of(all_graph.graph_1, 10)).suggest &&
+			all_graph.graph_1.at(index_of(all_graph.graph_1, 4)).suggest < all_graph.graph_1.at(index_of(all_graph.graph_1, 10)).suggest &&
 			is_unique(all_graph.graph_1)) 
 		{
+			for (size_t l = 0; l < 4; l++)
+			{
+				if (l == 1)
+					continue;
+				e[l].suggest =
+					all_graph.graph_1[index_of(all_graph.graph_1, l + 1)].suggest;
+			}
+			for (size_t l = 8; l < 10; l++)
+			{
+				e[l].suggest =
+					all_graph.graph_1[index_of(all_graph.graph_1, l + 1)].suggest;
+			}
 			to_break = true;
 		}
 		else
 		{
-			pop_unique_suggest(all_graph.graph_1);
+			pop_unique_suggest(all_graph.graph_1,e);
 		}
 	} while (!to_break);	
 	for (; i >= 0; i--)
 	{
 		pop_domain(all_graph, all_graph.graph_1[i].suggest);
 	}
-
 
 	to_break = false;
 	i = all_graph.graph_2.size() - 1;
@@ -328,45 +311,34 @@ void backtracking(graph &all_graph)
 			all_graph.graph_2[j].suggest =
 				all_graph.graph_2[j].domain_list[rand() % all_graph.graph_2[j].domain_list.size()];
 		}
-		if (all_graph.graph_2[i - 1].suggest < all_graph.graph_2[i].suggest &&
-			all_graph.graph_2[i - 2].suggest < all_graph.graph_2[i].suggest &&
-			is_unique(all_graph.graph_2)) 
+		if (is_unique(all_graph.graph_2)) 
 		{
+			for (int l = 1; l < 8; l++) {
+				if (l == 2 || l == 3)
+					continue;
+				e[l].suggest =
+					all_graph.graph_2[index_of(all_graph.graph_2, l+ 1)].suggest;
+			}
 			to_break = true;
 		}
 		else
 		{
-			pop_unique_suggest(all_graph.graph_2);
+			pop_unique_suggest(all_graph.graph_2, e);
+
 		}
 	} while (!to_break);
 	for (; i >= 0; i--)
 	{
 		pop_domain(all_graph, all_graph.graph_2[i].suggest);
-	}
 
-	to_break = false;
-	i = all_graph.graph_3.size() - 1;
-	do
+	}
+}
+
+void print_exam(exam* e) {
+	for (size_t i = 0; i < length; i++)
 	{
-		//2 5 6 7 8
-		srand(time(0));
-		for (int j = i; j >= 0; j--)
-		{
-			all_graph.graph_3[j].suggest =
-				all_graph.graph_3[j].domain_list[rand() % all_graph.graph_3[j].domain_list.size()];
-		}
-		if (is_unique(all_graph.graph_3))
-		{
-			to_break = true;
-		}
-		else
-		{
-			pop_unique_suggest(all_graph.graph_3);
-		}
-	} while (!to_break);
-	for (; i >= 0; i--)
-	{
-		pop_domain(all_graph, all_graph.graph_3[i].suggest);
+		cout << "E" << e[i].exam_order << " -> (t";
+		cout << get<0>(e[i].suggest) << "," << get<1>(e[i].suggest) << ")\n";
 	}
 }
 
@@ -384,9 +356,9 @@ int main()
 
 	graph_structure.update_queue(E);
 	
-	backtracking(graph_structure);
+	backtracking(graph_structure, E);
+		
+	print_exam(E);
 	
-	graph_structure.print_graph();
-
 	return 0;
 }
